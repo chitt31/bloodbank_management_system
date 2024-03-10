b0820f97151a64cb21e5c436b72d707d07b1c01b
Chitt Soni <139124423+chitt31@users.noreply.github.com>
2024-03-10 18:11:56 +0530

    Add files via upload

---
+++ admin/conn.php
@@ -0,0 +1,3 @@
+<?php
+$conn=mysqli_connect("localhost","root","1234","blood_donation") or die("Connection error");
+?>
---
+++ admin/session.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Rest of your head section -->
+</head>
+<body style="color: black">
+    <!-- Rest of your code -->
+</body>
+</html>
